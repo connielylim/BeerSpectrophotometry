02e2f93cab62c27d977f83dedb4b9889e1710a6b
Connie Lim <connielylim@gmail.com>
2016-12-06 17:31:15 -0800

    Add files via upload

---
+++ Spectrum Brewing/CaptureVC.swift
@@ -0,0 +1,128 @@
+//
+//  ViewController.swift
+//  Spectrum Brewing
+//
+//  Created by Connie Lim on 10/17/16.
+//  Copyright © 2016 Capstone Fall 2016. All rights reserved.
+//
+
+import UIKit
+
+class CaptureVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    var picker = UIImagePickerController()
+    var shutterButton = UIButton()
+    //Source: Tasting Beer by Randy Mosher
+    var pickerDataSource = ["Amber Ale 11-18",
+                            "American Pale Ale 6-14",
+                            "Baltic Porter 17-40",
+                            "Bavarian Weizen 4-10",
+                            "Belgian Strong Ale 4-7",
+                            "Bière de Garde 6-13",
+                            "Bitter, ESB 8-14",
+                            "Bock 15-30",
+                            "Dunkel Weizen 9-13",
+                            "English Brown Ale 12-22",
+                            "English Golden Ale 4-8",
+                            "Foreign Stout 30-65",
+                            "Imperial Pale Ale 5-11",
+                            "Imperial Stout 50-80",
+                            "Maibock 4-10",
+                            "Märzen 7-15",
+                            "Oatmeal Stout 25-40",
+                            "Oktoberfest 4-12",
+                            "Pale Ale 5-14",
+                            "Pilsner 2-7",
+                            "Porter 20-40",
+                            "Vienna Lager 7-14",
+                            "Witbier,Berliner Weisse 2-4"];
+    
+    //MARK: Picker View
+    @IBOutlet weak var BeerTypePicker: UIPickerView!
+    
+    //MARK: Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        picker.delegate = self
+        self.BeerTypePicker.dataSource = self
+        self.BeerTypePicker.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: UIPickerViewDataSource methods
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerDataSource.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        return pickerDataSource[row]
+    }
+    
+    //MARK: camera util related
+    func presentCamera(){
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else{
+            assert(false, "can't run without camera")
+        }
+        
+        picker.sourceType = .camera
+        picker.showsCameraControls = false
+        picker.cameraFlashMode = UIImagePickerControllerCameraFlashMode.off
+        
+        picker.cameraOverlayView = shutterButton
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func takePhoto() {
+        picker.takePicture()
+    }
+    
+    // MARK: ibaction
+    @IBAction func showCamera(_ sender: UIButton) {
+        presentCamera()
+        self.perform(#selector(takePhoto), with: nil, afterDelay: 3)
+        
+        
+    }
+    
+    func presentSRMResultVC(withVal SRMval:Double, withPic sampleImage:UIImage) {
+        //MARK: SRMResultVC presented
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        //let vc: UINavigationController = storyboard.instantiateViewController(withIdentifier: "SRMResultVC") as! UINavigationController
+        let vc = storyboard.instantiateViewController(withIdentifier: "SRMVC") as? SRMResultVC
+        
+        vc?.SRMnum = SRMval
+        let chosen = pickerDataSource[BeerTypePicker.selectedRow(inComponent: 0)]
+        vc?.beerTypeSRM = chosen
+        vc?.beerImage = sampleImage
+        UIApplication.shared.keyWindow?.rootViewController = vc
+        
+    }
+
+    
+    //MARK: UIImagePickerControllerDelegate methods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            let SRMval = getSRMValFrom(image: image)
+            
+            presentSRMResultVC(withVal:SRMval, withPic:image)
+            
+        }
+        
+       
+        
+        
+    }
+    
+}
+
---
+++ Spectrum Brewing/ColorUtils.swift
@@ -0,0 +1,60 @@
+//
+//  ColorUtils.swift
+//  Spectrum Brewing
+//
+//  Created by Connie Lim on 10/22/16.
+//  Copyright © 2016 Capstone Fall 2016. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreImage
+
+extension UIImage {
+    func getPixelColorBlue(pos: CGPoint = CGPoint(x: 0.0, y: 0.0)) -> CGFloat {
+        
+        if let pixelData = self.cgImage!.dataProvider!.data {
+            let data: UnsafePointer<UInt8> = CFDataGetBytePtr(pixelData)
+            
+            let pixelInfo: Int = ((Int(self.size.width) * Int(pos.y)) + Int(pos.x)) * 4
+            
+            //            let r = CGFloat(data[pixelInfo]) / CGFloat(255.0)
+            //            let g = CGFloat(data[pixelInfo+1]) / CGFloat(255.0)
+            let b = CGFloat(data[pixelInfo+2]) / CGFloat(255.0)
+            //            let a = CGFloat(data[pixelInfo+3]) / CGFloat(255.0)
+            
+            return b
+        } else {
+            assert(false, "unable to read pixel data")
+        }
+        return 0.0
+    }
+}
+
+func getSRMValFrom(image img:UIImage) -> Double{
+    let context = CIContext(options: nil)
+    
+    if let currentFilter = CIFilter(name: "CIAreaAverage") {
+        let beginImage = CIImage(image: img)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        currentFilter.setValue(CIVector.init(x: 0.0, y: 0.0, z: img.size.height, w: img.size.width), forKey: kCIInputExtentKey)
+        
+        if let output = currentFilter.outputImage {
+            if let cgimg = context.createCGImage(output, from: output.extent) {
+                let processedImage = UIImage(cgImage: cgimg)
+                // do something interesting with the processed image
+                return SRMValFrom(blueColor: processedImage.getPixelColorBlue())
+            }
+        }
+    }
+    print("white is returned because filter was not set or image could not be obtained from filter")
+    return 0.0
+}
+
+func SRMValFrom(blueColor bVal:CGFloat) -> Double{
+//    var absorbance = log(bVal/bVal)/log(10)
+    //here D,(dilution) is set to 1 meaning sample is not diluted
+//    var SRMVal = 12.7 * 1 * absorbance
+//    return Double(SRMVal)
+    return Double(bVal)
+}
